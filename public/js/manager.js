f08c00af8c5e5b106103e430873f41d29b36040b
jennifer yaffe <coolly6@walla.co.il>
2019-11-23 14:30:34 +0200

    ב

--- public/js/manager.js
+++ public/js/manager.js
@@ -1,4 +1,3 @@
-// getProjects()
 function addNewProject(e) {
     e.preventDefault();
     let projectName=e.target.projectname.value;
@@ -32,53 +31,55 @@ function addNewProject(e) {
             console.log('Fetch Error :-S', err)
         })
 }
-function getProjects() {
-    fetch('/getprojects')
-        .then(res => {
+// getProjects()
 
-            res.json().then(data => {
+// function getProjects() {
+//     fetch('/getprojects')
+//         .then(res => {
 
-                console.log("data",data)
-                let dataDOM = '';
-                let p1='';
-                let p2='';
-                for (let i = 0; i < data.length; i++) {
+//             res.json().then(data => {
+
+//                 console.log("data",data)
+//                 let dataDOM = '';
+//                 let p1='';
+//                 let p2='';
+//                 for (let i = 0; i < data.length; i++) {
                     
-                    if(i===0){
-                        dataDOM+=`<img src="${data[i].image}" alt="" style="width: 100%;height:30vw;" class="lastPic" >`
-                        p1=`<img src="${data[i].image}" alt="" style="width: 100%;height:30vw;"  >`
-                    }
+//                     if(i===0){
+//                         dataDOM+=`<img src="${data[i].image}" alt="" style="width: 100%;height:30vw;" class="lastPic" >`
+//                         p1=`<img src="${data[i].image}" alt="" style="width: 100%;height:30vw;"  >`
+//                     }
                     
-                    else{
-                        dataDOM+=`<img src="${data[i].image}" alt="" style="width: 100%;height:30vw;" >`
-                        if(i===1){
-                            p2=`<img src="${data[i].image}" alt= "" style="width: 100%;height:30vw;" class="firstPic" >`
-                        }
-                    }
+//                     else{
+//                         dataDOM+=`<img src="${data[i].image}" alt="" style="width: 100%;height:30vw;" >`
+//                         if(i===1){
+//                             p2=`<img src="${data[i].image}" alt= "" style="width: 100%;height:30vw;" class="firstPic" >`
+//                         }
+//                     }
                    
 
              
-                    // dataDOM += `<div class="project">
-                    // Project Name: <p>${data[i].projectName} </p>
-                    // Description: <p>${data[i].description}</p>
-                    // <hr>
-                    // <a href=${data[i].urlHeroku}>Visit Heroku</a>
-                    // <hr>
-                    // <a href=${data[i].urlGithub}>Visit Github</a>
-                    // <hr>
-                    // Image: <img src="${data[i].image}" height="50vw">
-                    // </div>`
-                }
-                dataDOM+=p1+p2;
-console.log(dataDOM)
+//                     // dataDOM += `<div class="project">
+//                     // Project Name: <p>${data[i].projectName} </p>
+//                     // Description: <p>${data[i].description}</p>
+//                     // <hr>
+//                     // <a href=${data[i].urlHeroku}>Visit Heroku</a>
+//                     // <hr>
+//                     // <a href=${data[i].urlGithub}>Visit Github</a>
+//                     // <hr>
+//                     // Image: <img src="${data[i].image}" height="50vw">
+//                     // </div>`
+//                 }
+//                 dataDOM+=p1+p2;
+// console.log(dataDOM)
 
-                let carouselSlide = document.querySelector('.carousel-slide');
-                carouselSlide.innerHTML = dataDOM;
+//                 let carouselSlide = document.querySelector('.carousel-slide');
+//                 carouselSlide.innerHTML = dataDOM;
 
-                // }).catch(function (err) {
-                //     console.log('Fetch Error :-S', err);
-                // })
-            })
+//                 // }).catch(function (err) {
+//                 //     console.log('Fetch Error :-S', err);
+//                 // })
+//             })
 
-        })
-}
+//         })
+// }
